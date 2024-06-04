ae22c8ff2564b3e6f8e4e6eac7ad7cd89286307a
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2024-06-04 20:52:49 +0700

    Initial commit

---
+++ jwt.demo/src/test/java/com/aq2208/jwt/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aq2208.jwt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
